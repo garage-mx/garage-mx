a9c7731168b9279c016cd38f71c7ef602a01daca
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2014-06-22 17:28:12 -0500

    Removed stupid messages

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express', olakase: "Ola k ase o te pego?"});
 });
 
-router.get('/olakase', function(req, res) {
-  res.render('olakase', { title: 'Express', olakase: "Ola k ase o te pego?"});
-});
-
 module.exports = router;
