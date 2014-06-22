ce5b3759122866b2f12407d4f221ace326f643cb
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2014-06-22 17:46:52 -0500

    Finished integration

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
