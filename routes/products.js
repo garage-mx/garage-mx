d49fb6b100a6d1c94f7ea3aaacb25257188741d6
Noel Alejandro <noel_ska15@hotmail.com>
2014-06-29 22:10:54 -0500

    Added Update, Create, and Delete methods

--- routes/products.js
+++ routes/products.js
@@ -25,29 +25,7 @@ var productSchema = mongoose.Schema({
 
 
 var Product = mongoose.model('Product', productSchema);
-
 /*
-// Creamos una instancia del objeto Product con información desde el inicio
-// We create a Product object instance with information from the beginning 
-var producto = new Product({ name: 'Disco duro', category: 'Computo', price: 1000 })
-console.log(producto.name +" $"+ producto.price);
-
-// Con esta funcion guardamos el producto en el modelo Product
-// This function save the prouducto object in Product model
-producto.save(function (err, producto) {
-  if (err) return console.error(err);
-  console.error("Guardado con exito");
-});
-/*
-
-/*
-// Con esta funcion elimino todos los productos de cierta categoria
-// This function is a delete instruction that remove all Products with a specified category in the Product model
-Product.remove({ category: 'Fotografia' }, function (err) {
-  if (err) return handleError(err);
-  // removed!
-});
-
 // Actualiza el producto con nombre 'Disco duro' y lo cambia a 'SSD'
 // Update the product named 'Disco duro' and change it to 'SSD'
 Product.update({ name: 'Disco duro' }, { name: 'SSD' }, function (err, numberAffected, raw) {
@@ -55,50 +33,62 @@ Product.update({ name: 'Disco duro' }, { name: 'SSD' }, function (err, numberAff
   console.log('The number of updated documents was %d', numberAffected);
   console.log('The raw response from Mongo was ', raw);
 });
+
 */
-// Esto se trae todos los registros del modelo Product
-// This function is like a select query that show all the productos of Product model 
-exports.funciones = function()
-{
-  var valor = null;
-  return {
-    productsList: function(result) 
-    {
-      Product.find(function (err, productos) {
-        if (err) return console.error(err);
-        valor = JSON.stringify(productos);
-        console.log(productos);
-        result(valor);
-      });
-    }
-  }
-}
 
-/*
-// Busca el producto con nombre 'SSD' y obtiene el contenido
-// Search the product named 'SSD' and get the respective object 
-Product.findOne({ name: 'SSD' }, function (err, doc) {
-  if (err) return console.error(err);
-  console.log(doc);
-});*/
-
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('products/index', { title: 'Productos' });
+router.get('/new', function(req, res) {
+    res.render('products/new', { title: 'Nuevo Producto' });
 });
 
-router.route('/new')
-  .get(function(req, res, next) {
-    res.render('products/new', { title: 'Nuevo Productos' });
-  })
-  .post(function(req, res, next) {
-    var product = req.param('product');
-    console.log(product.price);
-    res.redirect('/products/list');
+router.post('/new', function(req, res) {
+  var product = req.param('product');
+  // Creamos una instancia del objeto Product con información desde el inicio
+  // We create a Product object instance with information since beginning 
+  var producto = new Product({ name: product.name, category: product.category, price: product.price })
+
+  // Con esta funcion guardamos el producto en el modelo Product
+  // This function save the prouducto object in Product model
+  producto.save(function (err, producto) {
+    if (err) return console.error(err);
+    console.error("Guardado con exito");
   });
-
-router.get('/list',function(req, res){
-  console.log(exports.funciones.productsList(function(data){ console.log(data); }));
-  res.render('products/list', { title: 'Lista de productos', products: exports.productsList() });
+  res.redirect('/products');
 });
+
+router.get('/',function(req, res){
+  // Esto se trae todos los registros del modelo Product
+  // This function is like a select query that show all the productos of Product model 
+  Product.find(function (err, productos) {
+    if (err) return console.error(err);
+    res.render('products/list', { title: 'Productos', products: productos });
+  });
+});
+
+router.get('/update/:id', function(req, res){
+  // Busca el producto con id enviado por el usuario y obtiene el contenido
+  // Search the product envoy by user and get the respective object 
+  Product.findById(req.param('id'), function (err, producto) {
+    if (err) return console.error(err);
+    res.render('products/update', { title: 'Actualizar producto', product: producto });
+  });
+});
+
+router.post('/update', function(req, res){
+  // Busca el producto con id enviado por el usuario y obtiene el contenido
+  // Search the product envoy by user and get the respective object 
+  Product.update({ _id: req.param('id') }, {$set: req.param('product')}, function (err, producto) {
+    if (err) return console.error(err);
+    res.redirect('/products');
+  });
+});
+
+router.get('/delete/:id',function(req, res){
+  // Con esta funcion elimino todos los productos de cierta categoria
+  // This function is a delete instruction that remove all Products with a specified category in the Product model
+  Product.remove({ _id: req.param('id') }, function (err) {
+    if (err) return handleError(err);
+    res.redirect('/products');
+  });
+});
+
 module.exports = router;
