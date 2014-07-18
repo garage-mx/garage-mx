86ea317db360fc05428817a9fdd4b87d310c068b
Noel Alejandro <noel_ska15@hotmail.com>
2014-07-18 00:29:55 -0500

    First login try with passport

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', olakase: "Ola k ase o te pego?"});
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
